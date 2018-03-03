61017d7e95de2abf03b46c870beaef81894b6305
Carol Enache <enache.carol@gmail.com>
2018-03-03 20:22:12 +0000

    fix the jquery issue and make use of fetch and promise to get access to the data

--- sketch.js
+++ sketch.js
@@ -1,13 +1,63 @@
 'use strict'
 
+
+const fetchOption = {
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    mode: 'cors'
+}
+
 $('form').on('submit', function(e) {
     e.preventDefault();
 
     let types = $('input[type=text]').val().replace(/\s/g,'');
     types = types.split(',');
-    console.log(types);
+
+    let trainerTypeCalls = types.map( elem => {
+        return fetch(`http://pokeapi.salestock.net/api/v2/type/${elem}/`, fetchOption)
+    })
+
+    getPromiseData(trainerTypeCalls)
+        .then(result => {
+            getDoubleDamagePokemon(result)
+        })
+    
+    function getDoubleDamagePokemon(pokemonTypes) {
+
+        pokemonTypes = pokemonTypes
+            .map(types => types.damage_relations.double_damage_from)
+            
+
+        let mergedPokemonTypes = [].concat.apply([], pokemonTypes);
+        console.log(mergedPokemonTypes)
+        }
+    
+    function getPromiseData(promisesArray) {
+        return new Promise((resolve, reject) => {
+            Promise
+                .all(promisesArray)
+                .then(res => {
+                    return res.map(type => type.json())
+                })
+                .then(res => {
+                    Promise
+                        .all(res)
+                        .then(resolve)
+                })
+                .catch(reject);
+        })
+    }
+
 
 });
+
+
+
+
+
+
+
 
 function displayPokemon(pokemon) {
     // loop through and display the pokemon !
