7965e839f2ea66c447857e8283d8c506a9f8b729
Carol Enache <enache.carol@gmail.com>
2018-03-04 00:12:23 +0000

    done

--- sketch.js
+++ sketch.js
@@ -16,48 +16,75 @@ $('form').on('submit', function(e) {
 
     let trainerTypeCalls = types.map( elem => {
         return fetch(`http://pokeapi.salestock.net/api/v2/type/${elem}/`, fetchOption)
-    })
+    });
 
     getPromiseData(trainerTypeCalls)
-        .then(result => {
-            getDoubleDamagePokemon(result)
+        .then((result) => {
+            console.log(result)
+            getDoubleDamagePokemon(result);
+
         })
-    
-    function getDoubleDamagePokemon(pokemonTypes) {
-
-        pokemonTypes = pokemonTypes
-            .map(types => types.damage_relations.double_damage_from)
-            
-
-        let mergedPokemonTypes = [].concat.apply([], pokemonTypes);
-        console.log(mergedPokemonTypes)
-        }
-    
-    function getPromiseData(promisesArray) {
-        return new Promise((resolve, reject) => {
-            Promise
-                .all(promisesArray)
-                .then(res => {
-                    return res.map(type => type.json())
-                })
-                .then(res => {
-                    Promise
-                        .all(res)
-                        .then(resolve)
-                })
-                .catch(reject);
-        })
-    }
-
-
 });
 
+function getDoubleDamagePokemon(pokemonTypes) {
 
+    pokemonTypes =   pokemonTypes
+        .map( types => {
+            return types.damage_relations.double_damage_from 
+        })
+        .reduce(flatten, [])
+        .map(type => {
+            return fetch(type.url, fetchOption)
+        })
 
+        getPromiseData(pokemonTypes)
+            .then(results => {
+                buildTeam(results)
+            })
+}
 
+function buildTeam(pokemons) {
+    let team = [];
+    pokemons = pokemons
+        .map( pokemon => {
+            return pokemon.pokemon;
+        })
+        .reduce(flatten, [])
+        .map(pokemon => pokemon.pokemon)
+        
+        for(let i = 0; i < 6; i++){
+            team.push( getRandomPokemon(pokemons) )
+        }
 
+        team = team.map(pokemon => {
+            return fetch(pokemon.url, fetchOption);
+        })
 
+        getPromiseData(team)
+            .then(pokemonData => {
+                displayPokemon(pokemonData)
+            })
+}
 
+function getRandomPokemon(pokemonArray) {
+    return pokemonArray[ Math.floor(Math.random() * pokemonArray.length) ];
+}
+
+const flatten = (a, b) => [...a, ...b];
+
+function getPromiseData(promisesArray) {
+    return new Promise((resolve, reject) => {
+        Promise.all(promisesArray)
+            .then(res => {
+                return res.map( type => type.json());
+            })
+            .then( res => {
+                Promise.all(res)
+                    .then(resolve)
+            })
+            .catch(reject)
+    })
+}
 
 function displayPokemon(pokemon) {
     // loop through and display the pokemon !
